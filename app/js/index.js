bbf122820ef785cf9b7307cf42566e79d0af5f9e
webarata3 <webarata3@gmail.com>
2016-08-15 19:00:00 +0900

    リファクタリング

--- app/js/index.js
+++ app/js/index.js
@@ -5,17 +5,20 @@ const electron = require('electron');
 // http://qiita.com/taizo/items/3a5505308ca2e303c099
 const moment = require('moment');
 
-const appValidator = require('../js/appValidator');
+const appValidator = require('../js/appValidator.js');
+const dbManager = require('../js/dbManager.js');
 
 const controller = {};
-const dbManager = {};
 
-//******************************
-// 描画用のオブジェクト
-//******************************
 controller.init = () => {
   // 初期設定
-  dbManager.init();
+  const promise = dbManager.init();
+
+  promise.then(() => {
+    controller.renderWeightList();
+  }).catch(() => {
+    // TODO
+  });
 
   const $height = document.getElementById('height');
   // イベント
@@ -72,98 +75,6 @@ controller.renderWeightList = () => {
     document.getElementsByClassName('content')[0].style.height = `${window.innerHeight - 50}px`;
   }).catch(() => {
     // TODO
-  });
-};
-
-//******************************
-// DB管理用のオブジェクト
-//******************************
-dbManager.indexedDB = window.indexedDB;
-dbManager.db = null;
-
-// 初期化、アップグレード時の処理
-dbManager.init = () => {
-  const request = indexedDB.open('weightManager', 1);
-
-  // DBのバージョンが上がった場合
-  request.onupgradeneeded = event => {
-    dbManager.db = event.target.result;
-    dbManager.db.createObjectStore('weight', {keyPath: 'date'});
-  };
-
-  // DBのオープンが成功した場合
-  request.onsuccess = event => {
-    dbManager.db = event.target.result;
-    // 非同期だからうーん。
-    // TODO
-    controller.renderWeightList();
-  };
-};
-
-// 全件読み込み
-dbManager.readAll = () => {
-  const weightList = [];
-
-  const tx = dbManager.db.transaction(['weight'], 'readonly');
-
-  const weightStore = tx.objectStore('weight');
-  const cursorRequest = weightStore.openCursor();
-  const promise = new Promise((resolve, reject) => {
-    cursorRequest.onsuccess = event => {
-      var cursor = event.target.result;
-      if (cursor) {
-        const date = cursor.key;
-        const weight = Number.parseFloat(cursor.value.weight);
-        const dispDate = moment(date).format('YYYY/MM/DD');
-        weightList.push({
-          date: dispDate,
-          weight: weight
-        });
-
-        cursor.continue();
-      } else {
-        // 読み込みの終了
-        // ウィンドウの高さの決定
-        // TODO 仮
-        document.getElementsByClassName('content')[0].style.height = `${window.innerHeight - 50}px`;
-        resolve(weightList);
-      }
-    };
-    cursorRequest.onerror = event => {
-      // TODO
-      console.log('ERROR');
-    }
-  });
-
-  return promise;
-};
-
-dbManager.insert = (date, weight, completeCallback, errorCallback) => {
-  new Promise((resolve, reject) => {
-    // キー情報の読み込み
-    const tx = dbManager.db.transaction(['weight'], 'readwrite');
-    const store = tx.objectStore('weight');
-    const request = store.get(date);
-    request.onsuccess = function(event) {
-      const value = event.target.result;
-      // すでに登録済みの場合エラー
-      if (value != null) {
-        document.getElementById('weightError').innerText = '指定の日の体重は登録済みです'
-      }
-      store.put({date: date, weight: weight});
-      tx.oncomplete = function() {
-        resolve();
-      };
-      tx.onerror = function(event) {
-        reject();
-      };
-    };
-  }).then(() => {
-    completeCallback();
-  }).catch((error) => {
-    // TODO エラー処理
-    console.log(error);
-    errorCallback(error);
   });
 };
 
