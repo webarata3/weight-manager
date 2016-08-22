a7df7f2e51838d76d7c53bc7b612cbbd5fedb767
荒田真一 <arata@mbp.local>
2016-08-22 12:59:41 +0900

    Graph画面への切り替えの追加

--- js/view/app-view.js
+++ js/view/app-view.js
@@ -13,8 +13,17 @@ class AppView extends View {
     this._weightListModel = weightListModel;
 
     this._setEl({
+      'mainButton': '_$mainButton',
+      'graphButton': '_$graphButton',
+      'mainContainer': '_$mainContainer',
+      'graphContainer': '_$graphContainer',
       'insertFieldset': '_$insertFieldset',
       'updateFieldset': '_$updateFieldset'
+    });
+
+    this._setEvent({
+      'click mainButton': this._onClickMainButton,
+      'click graphButton': this._onClickGraphButton
     });
 
     this._setAppEvent(this._inputHeightModel, {
@@ -54,6 +63,20 @@ class AppView extends View {
   _changeInsertMode(param) {
     this._$insertFieldset.classList.remove('hide');
     this._$updateFieldset.classList.add('hide');
+  }
+
+  _onClickMainButton() {
+    this._$mainButton.classList.add('active');
+    this._$graphButton.classList.remove('active');
+    this._$mainContainer.classList.remove('hide');
+    this._$graphContainer.classList.add('hide');
+  }
+
+  _onClickGraphButton() {
+    this._$mainButton.classList.remove('active');
+    this._$graphButton.classList.add('active');
+    this._$mainContainer.classList.add('hide');
+    this._$graphContainer.classList.remove('hide');
   }
 }
 
