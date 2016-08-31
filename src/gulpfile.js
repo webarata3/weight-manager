0af9caeca948cfcf67204f5ae8cc4109d83e2df3
webarata3 <webarata3@gmail.com>
2016-08-31 12:03:09 +0900

    ESLintの調整

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -1,16 +1,9 @@
 var gulp = require('gulp');
 var eslint = require('gulp-eslint');
-var notify = require('gulp-notify');
-var plumber = require('gulp-plumber');
-
 
 gulp.task('eslint', function() {
   gulp.src('js/**/*.js')
-    .pipe(plumber({
-      errorHandler: notify.onError('Error: <%= error.message %>')
-    }))
     .pipe(eslint())
     .pipe(eslint.format())
-    .pipe(eslint.failAfterError())
-    .pipe(gulp.dest('app/product/js/'));
+    .pipe(eslint.failAfterError());
 });
