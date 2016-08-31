84b50624f0ae79870061b2abc14836a4aa7100b1
webarata3 <webarata3@gmail.com>
2016-08-31 14:55:24 +0900

    テスト

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -3,8 +3,5 @@ var eslint = require('gulp-eslint');
 
 gulp.task('eslint', function() {
   gulp.src('js/**/*.js')
-    .pipe(eslint())
-    .pipe(eslint.format())
-    .pipe(eslint.failAfterError())
-    .on('error', process.exit.bind(process, 1));
+    .pipe(eslint());
 });
